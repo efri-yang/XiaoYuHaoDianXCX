5acb9d94c3bad9d7be4f0d684c0374a5dc9cccb6
efri-yang <948061564@qq.com>
2018-04-18 11:10:59 +0800

    c

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,6 +1,41 @@
+import WxValidate from '../../static/js/plugin/WxValidate'
 const app = getApp();
 Page({
+  onLoad:function(){
+    this._initValidate();
+  },
   formSubmit: function (e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    const params = e.detail.value;
+    console.log(params);
+    if (!this.WxValidate.checkForm(e)) {
+      const error = this.WxValidate.errorList[0]
+      wx.showToast({
+        title: error.msg,
+        icon: 'none',
+        duration: 2000
+      })  
+      return false
+    }
+  },
+  _initValidate() {
+    const rules={
+      username:{
+        required: true
+      },
+      password:{
+        required: true
+      }
+    }
+
+    const messages = {
+      username: {
+        required: "请输入账号"
+      },
+      password: {
+        required: "请输入密码"
+      }
+    }
+
+    this.WxValidate = new WxValidate(rules, messages);
   }
 })
